6e48b0bcd565c4af5852e53cbb95ef53a8b306db
Nisal Pubudu <nisal.pubudu@gmail.com>
2021-06-15 21:08:45 +0530

    completed upto 7 videos

---
+++ MicroService_Practices/video 07/profile-service/src/main/java/com/nisal/rentcloud/profileservice/service/CustomerService.java
@@ -0,0 +1,8 @@
+package com.nisal.rentcloud.profileservice.service;
+
+import com.nisal.rentacloud.commons.model.Customer;
+
+public interface CustomerService {
+
+    Customer save(Customer customer);
+}
