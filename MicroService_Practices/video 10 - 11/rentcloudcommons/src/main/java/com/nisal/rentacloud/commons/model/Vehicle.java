59866307019114ef17ea3562e99d26e785814c30
Nisal Pubudu <nisal.pubudu@gmail.com>
2021-06-17 04:55:26 +0530

    completed upto 11 videos

--- MicroService_Practices/video 10 - 11/rentcloudcommons/src/main/java/com/nisal/rentacloud/commons/model/Vehicle.java
+++ MicroService_Practices/video 10 - 11/rentcloudcommons/src/main/java/com/nisal/rentacloud/commons/model/Vehicle.java
@@ -1,9 +1,6 @@
 package com.nisal.rentacloud.commons.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "vehicle")
