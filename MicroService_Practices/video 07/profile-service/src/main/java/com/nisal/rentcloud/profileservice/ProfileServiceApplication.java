91825f0c619ebd7a1d45f8e965c57fed7965d56d
Nisal Pubudu <nisal.pubudu@gmail.com>
2021-06-16 22:13:15 +0530

    *Major update on profile-service
    Removed config server & add configuration within the service| Downgraded springboot version to 2.3.12

--- MicroService_Practices/video 07/profile-service/src/main/java/com/nisal/rentcloud/profileservice/ProfileServiceApplication.java
+++ MicroService_Practices/video 07/profile-service/src/main/java/com/nisal/rentcloud/profileservice/ProfileServiceApplication.java
@@ -3,9 +3,11 @@ package com.nisal.rentcloud.profileservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.nisal.rentacloud.commons.model")
+@EnableResourceServer
 public class ProfileServiceApplication {
 
 	public static void main(String[] args) {
