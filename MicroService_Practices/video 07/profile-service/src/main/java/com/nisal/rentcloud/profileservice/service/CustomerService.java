e3ce27e02c027d439e7124d0355cda907f8aabef
Nisal Pubudu <nisal.pubudu@gmail.com>
2021-06-16 20:49:14 +0530

    add 2 methods to fetch all users & fetch by Id

--- MicroService_Practices/video 07/profile-service/src/main/java/com/nisal/rentcloud/profileservice/service/CustomerService.java
+++ MicroService_Practices/video 07/profile-service/src/main/java/com/nisal/rentcloud/profileservice/service/CustomerService.java
@@ -2,7 +2,13 @@ package com.nisal.rentcloud.profileservice.service;
 
 import com.nisal.rentacloud.commons.model.Customer;
 
+import java.util.List;
+
 public interface CustomerService {
 
     Customer save(Customer customer);
+
+    Customer fetchById(int profileId);
+
+    List<Customer> fetchAllProfiles();
 }
--- MicroService_Practices/video 07/profile-service/src/main/java/com/nisal/rentcloud/profileservice/service/CustomerServiceImpl.java
+++ MicroService_Practices/video 07/profile-service/src/main/java/com/nisal/rentcloud/profileservice/service/CustomerServiceImpl.java
@@ -5,6 +5,9 @@ import com.nisal.rentcloud.profileservice.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CustomerServiceImpl implements CustomerService{
 
@@ -14,5 +17,20 @@ public class CustomerServiceImpl implements CustomerService{
     @Override
     public Customer save(Customer customer) {
         return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer fetchById(int profileId) {
+        Optional<Customer> customer = customerRepository.findById(profileId);
+        if(customer.isPresent()){
+            return customer.get();
+        }else {
+            return null;
+        }
+    }
+
+    @Override
+    public List<Customer> fetchAllProfiles() {
+        return customerRepository.findAll();
     }
 }
