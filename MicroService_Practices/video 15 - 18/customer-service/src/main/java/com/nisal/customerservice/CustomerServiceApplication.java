3d92e6aad78f41f07732abe317abb14c4dd5e118
Nisal Pubudu <nisal.pubudu@gmail.com>
2021-06-18 14:08:30 +0530

    completed upto 16 videos

--- MicroService_Practices/video 15 - 18/customer-service/src/main/java/com/nisal/customerservice/CustomerServiceApplication.java
+++ MicroService_Practices/video 15 - 18/customer-service/src/main/java/com/nisal/customerservice/CustomerServiceApplication.java
@@ -3,10 +3,12 @@ package com.nisal.customerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 //note -> video 15 -> 07:52
 @EntityScan(basePackages = "com.nisal.model.customer")
+@EnableEurekaClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
--- MicroService_Practices/video 15 - 18/vehicle-service/src/main/java/com/nisal/vehicleservice/VehicleServiceApplication.java
+++ MicroService_Practices/video 15 - 18/vehicle-service/src/main/java/com/nisal/vehicleservice/VehicleServiceApplication.java
@@ -3,9 +3,11 @@ package com.nisal.vehicleservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.nisal.model.vehicle")
+@EnableEurekaClient
 public class VehicleServiceApplication {
 
 	public static void main(String[] args) {
