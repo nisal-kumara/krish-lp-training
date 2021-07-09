63e03c7acca00fffeadacbdf334d6a9c787016f7
Nisal Pubudu <nisal.pubudu@gmail.com>
2021-07-09 10:43:08 +0530

    created a rest-service that exposed directors' details

---
+++ GraphQL_With_NodeJS/director_rest_service/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const directors = require('./data/directors.json');
+
+const app = express();
+
+//get all directors
+app.get("/directors", (req, res) => {
+    res.send(directors)
+})
+
+//get a director by ID
+app.get("/directors/:id", (req, res) => {
+    res.send(directors.filter(dir => {
+        return dir.id == req.params.id
+    }))
+})
+app.listen(3000, () => console.log("Server is running on 3000"))
